809c4831691e4e4e9cfa6dbb24c69a63098cb093
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-09-18 21:05:06 +0300

    add personal account URL and test user configuration

--- config.py
+++ config.py
@@ -4,3 +4,10 @@ BASE_URL = 'https://stellarburgers.nomoreparties.site/'
 
 LOGIN_URL = urljoin(BASE_URL, 'login')
 REGISTER_URL = urljoin(BASE_URL, 'register')
+PERSONAL_ACCOUNT_URL = urljoin(BASE_URL, 'account/profile')
+
+TEST_USER = {
+    "name": "novohudonoser",
+    "email": "novohudonoser@mail.ru",
+    "password": "qwerqwer"
+}
