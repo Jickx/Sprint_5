06663b56f5199f3053d5f0cbfc1828b2f100c9d9
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-09-18 21:05:33 +0300

    add locators for login, recovery, and profile pages

--- locators.py
+++ locators.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     PERSONAL_ACCOUNT_BUTTON = (By.XPATH, "//p[text()='Личный Кабинет']")  # TODO
     MAKE_ORDER_BUTTON = (By.XPATH, "//button[text()='Оформить заказ']")
+    LOGIN_TO_ACCOUNT_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")
+    MAKE_A_BURGER_TITLE = (By.XPATH, "")
 
 
 class LoginPageLocators:
@@ -11,6 +13,7 @@ class LoginPageLocators:
     PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']//following-sibling::input")
     LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти'] ")
     REGISTER_LINK = (By.XPATH, "//a[@href='/register']")
+    RECOVER_LINK = (By.XPATH, "//a[@href='/forgot-password']")
 
 
 class RegisterPageLocators:
@@ -19,3 +22,14 @@ class RegisterPageLocators:
     PASSWORD_INPUT = (By.XPATH, "//label[text()='Пароль']//following-sibling::input")
     REGISTER_BUTTON = (By.XPATH, "//button[text()='Зарегистрироваться']")
     ERROR_MESSAGE = (By.CSS_SELECTOR, ".input__error")
+    LOGIN_LINK = (By.XPATH, "//a[@href='/login']")
+
+
+class RecoveryPageLocators:
+    LOGIN_LINK = (By.XPATH, "//a[@href='/login']")
+
+
+class ProfilePageLocators:
+    PROFILE_LINK = (By.XPATH, "//a[@href='/account/profile']")
+    CONSTRUCT_BUTTON = (By.XPATH, "//li/a[@href='/']")
+    LOGO_LINK = (By.XPATH, "//div/a[@href='/']")
