d4ec01c8e29f2b4a3a7d525c9f62fe1b59cb07b5
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-09-18 21:19:44 +0300

    add logout test for personal account functionality

---
+++ tests/test_logout.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from config import LOGIN_URL
+from locators import (
+    MainPageLocators,
+    LoginPageLocators,
+    ProfilePageLocators
+)
+
+
+class TestLogout:
+    def test_logout_personal_account(self, existing_user, driver):
+        """Проверка выхода по кнопке «Выйти» в личном кабинете."""
+        driver.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
+        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(existing_user['email'])
+        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(existing_user['password'])
+        driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
+
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located(
+                MainPageLocators.PERSONAL_ACCOUNT_BUTTON
+            )
+        ).click()
+
+        WebDriverWait(driver, 3).until(
+            expected_conditions.visibility_of_element_located(
+                ProfilePageLocators.EXIT_BUTTON
+            )
+        ).click()
+
+        assert driver.current_url == LOGIN_URL
