3b37d3eca2fe82e33ebf2ac5768c57889c3c67f3
Valentin Sharonov <valentin.sharonov@gmail.com>
2025-09-19 19:51:02 +0300

    Remove "if" statements to maintain atomicity

--- tests/test_ingredient_tabs.py
+++ tests/test_ingredient_tabs.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -7,42 +8,42 @@ from locators import BurgerIngredientLocators
 class TestIngredientsTabs:
     def test_click_bun_tab_scrolls_to_buns(self, driver):
         """Проверка перехода в раздел Булки при нажатии вкладки Булки."""
-        active_text = driver.find_element(*BurgerIngredientLocators.ACTIVE_TAB).text
-        if active_text != "Булки":
-            driver.find_element(*BurgerIngredientLocators.BUNS_TAB).click()
+        element = WebDriverWait(driver, 5).until(
+            expected_conditions.visibility_of_element_located(BurgerIngredientLocators.BUNS_TAB)
+        )
+
+        ActionChains(driver).move_to_element(element).click().perform()
 
         WebDriverWait(driver, 5).until(
-            expected_conditions.visibility_of_element_located(
-                BurgerIngredientLocators.BUNS_SECTION
-            )
+            expected_conditions.visibility_of_element_located(BurgerIngredientLocators.BUNS_SECTION)
         )
 
         assert driver.find_element(*BurgerIngredientLocators.ACTIVE_TAB).text == "Булки"
 
     def test_click_sauce_tab_scrolls_to_sauces(self, driver):
         """Проверка перехода в раздел Соусы при нажатии вкладки Соусы."""
-        active_text = driver.find_element(*BurgerIngredientLocators.ACTIVE_TAB).text
-        if active_text != "Соусы":
-            driver.find_element(*BurgerIngredientLocators.SAUCES_TAB).click()
+        element = WebDriverWait(driver, 5).until(
+            expected_conditions.visibility_of_element_located(BurgerIngredientLocators.SAUCES_TAB)
+        )
+
+        ActionChains(driver).move_to_element(element).click().perform()
 
         WebDriverWait(driver, 5).until(
-            expected_conditions.visibility_of_element_located(
-                BurgerIngredientLocators.SAUCES_SECTION
-            )
+            expected_conditions.visibility_of_element_located(BurgerIngredientLocators.SAUCES_SECTION)
         )
 
         assert driver.find_element(*BurgerIngredientLocators.ACTIVE_TAB).text == "Соусы"
 
     def test_click_filling_tab_scrolls_to_fillings(self, driver):
         """Проверка перехода в раздел Начинки при нажатии вкладки Начинки."""
-        active_text = driver.find_element(*BurgerIngredientLocators.ACTIVE_TAB).text
-        if active_text != "Начинки":
-            driver.find_element(*BurgerIngredientLocators.FILLINGS_TAB).click()
+        element = WebDriverWait(driver, 5).until(
+            expected_conditions.visibility_of_element_located(BurgerIngredientLocators.FILLINGS_TAB)
+        )
+
+        ActionChains(driver).move_to_element(element).click().perform()
 
         WebDriverWait(driver, 5).until(
-            expected_conditions.visibility_of_element_located(
-                BurgerIngredientLocators.FILLINGS_SECTION
-            )
+            expected_conditions.visibility_of_element_located(BurgerIngredientLocators.FILLINGS_SECTION)
         )
 
         assert driver.find_element(*BurgerIngredientLocators.ACTIVE_TAB).text == "Начинки"
